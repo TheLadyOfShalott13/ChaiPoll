de30c7575444eaf839a6b62a9ab2068d6fa659a8
klavox1434 <angel.locsin.fan95@gmail.com>
2024-10-29 05:55:05 +0400

    Initial commit - adding all client related files including pages, components, options

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import {
+    useEffect,
+    useMemo,
+} from "react"
+import Bar from "../components/BarChart.jsx"
+import { io } from 'socket.io-client'
+
+function Home() {
+    const socket = useMemo(() =>
+        io('http://localhost:5000'));
+
+    useEffect(() => {
+
+        socket.on('connect', () =>
+            console.log(socket.id))
+
+        socket.on('connect_error', () => {
+            setTimeout(() =>
+                socket.connect(), 5000)
+        })
+        return () => {
+            socket.off('connect', () =>
+                console.log("connected"));
+            socket.off('disconnect', () =>
+                console.log("connected"));
+        };
+    }, [])
+
+    return (
+        <>
+            <img src="./assets/chailogo.png"/><h1>Real-Time Chai Polling App</h1>
+            <Bar socket={socket} />
+        </>
+    )
+}
+
+export default Home
